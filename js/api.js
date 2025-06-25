ab5ee143f02d0669d88065c379ace4b292820481
regensv <regensv@gmail.com>
2025-06-25 20:09:37 +0300

    fix

--- js/api.js
+++ js/api.js
@@ -50,3 +50,30 @@ async function APIGetCoins(initData) {
         return 0;
     }
 }
+
+async function APISetCoins(initData, coins) {
+    const url = 'https://scg.rain.dp.ua/api/user/set/coins';
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                initData: initData,
+                totalCoins: coins,
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Request error:', error);
+        return 0;
+    }
+}
