4ced2ba6b5ee9e8df1f4ed691e535e3ef0897d2f
regensv <regensv@gmail.com>
2025-06-25 18:50:26 +0300

    test get coins

--- js/api.js
+++ js/api.js
@@ -1,25 +1,26 @@
-function APIGetCoins(initData) {
+async function APIGetCoins(initData) {
     const url = 'https://scg.rain.dp.ua/api/user/get/coins';
 
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            initData: initData,
-        }),
-    })
-    .then(response => {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                initData: initData,
+            }),
+        });
+
         if (!response.ok) {
             throw new Error(`HTTP Error: ${response.status}`);
         }
-        return response.json();
-    })
-    .then(data => {
-        return data.result
-    })
-    .catch(error => {
+
+        const data = await response.json();
+
+        return data.result;
+    } catch (error) {
         console.error('Request error:', error);
-    });
+        return 0;
+    }
 }
